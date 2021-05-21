fd273f9844f0f35a4cae5d37772fb521bd608c27
Silas Wieberneit <silas.ne.india@gmail.com>
2021-05-21 06:21:55 +0200

    Use yarn instead of npm

--- cli/util.js
+++ cli/util.js
@@ -126,7 +126,7 @@ function modExists(name, version) {
 }
 
 function modInstall(install) {
-    child_process.execSync("npm --silent install " + (typeof install === "string" ? install : install.join(" ")), {
+    child_process.execSync("yarn --silent add " + (typeof install === "string" ? install : install.join(" ")), {
         cwd: __dirname,
         stdio: "ignore"
     });
